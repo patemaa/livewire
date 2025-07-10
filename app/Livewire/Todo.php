7875a2c9c43c5f213454041a20de72771e4c5633
Fatma R <anthoschichequs@gmail.com>
2025-07-10 11:45:13 +0300

    lifecycle hooks: mount

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -7,15 +7,22 @@ use Livewire\Component;
 class Todo extends Component
 {
     public $todo = '';
-    public $todos = [
-        'clean kitchen',
-        'read books',
-        'bake a cake'
-    ];
+    public $todos = [];
+
+    public function mount()
+    {
+        $this->todos = [
+            'clean kitchen',
+            'read books',
+            'bake a cake',
+        ];
+    }
+
     public function render()
     {
         return view('livewire.todo');
     }
+
     public function add()
     {
         $this->todos[] = $this->todo;
